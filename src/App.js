24e057297bf8639d30bc2552770d885e63087bea
limengting <350056846@qq.com>
2017-08-03 16:24:01 +0800

    更新TodoItem 的样式

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
           onChange={this.changeTitle.bind(this)}
           onSubmit={this.addTodo.bind(this)}/>
         </div>
-         <ol>
+         <ol className="todoList">
           {todu}
         </ol> 
       </div>
