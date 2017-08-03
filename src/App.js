64970c3812e53ff41633df2722e256b8901b943b
limengting <350056846@qq.com>
2017-08-03 15:43:24 +0800

    删除

--- src/App.js
+++ src/App.js
@@ -15,7 +15,10 @@ class App extends Component {
   }
   render() {
     let todu = this.state.todoList.map((item,index)=>{
-      return ( <li key={index}><TodoItem todo = {item} onToggle={this.toggle.bind(this)}/></li>);
+      return ( <li key={index}>
+              <TodoItem todo = {item} onToggle={this.toggle.bind(this)}
+              onDelete={this.delete.bind(this)}/>
+               </li>);
     })
    
     return (
@@ -32,6 +35,10 @@ class App extends Component {
       </div>
     )
   }
+    delete(event,todo){
+      todo.delete=true
+      this.setState(this.state)
+    }
     toggle(e,todo){
       todo.status = todo.status === 'completed'?'':'completed'
       this.setState(this.state)
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -4,11 +4,14 @@ export default class TodoItem extends Component{
         return (
             <div>
                 <input type="checkbox" checked={this.props.todo.status === 'completed'}
-                onChange={this.toggle.bind(this)}
-                />
+                onChange={this.toggle.bind(this)}  />
+                <button onClick={this.delete.bind(this)}>删除</button>
                 {this.props.todo.title}
             </div>
         )
+    }
+    delete(e){
+        this.props.onDelete(e,this.props.todo)
     }
     toggle(e){
         this.props.onToggle(e,this.props.todo)
